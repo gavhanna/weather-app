3dbb5ccc9c35177ca776addb3b5ce6d4e5de910b
gavhanna <gavhanna@gmail.com>
2016-03-13 00:54:05 +0000

    adding 8 day forecast

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 	    var longi = position.coords.longitude;
 			var url = "https://api.forecast.io/forecast/e9b17063eb0eb18dd082154493fd5361/" + lati + "," + longi;
 			// Function for converting UNIX to human-readable time!
-			function convertTimestamp(timestamp) {
+			function UNIXtoHour(timestamp) {
 				var d = new Date(timestamp * 1000),	// Convert the passed timestamp to milliseconds
 					hh = d.getHours(),
 					h = hh,
@@ -24,6 +24,33 @@ $(document).ready(function(){
 				}
 				time = h + ':' + min + ' ' + ampm;	
 				return time;
+				}
+
+				function UNIXtoDay(timestamp) {
+				  var d = new Date(timestamp * 1000),	// Convert the passed timestamp to milliseconds
+						yyyy = d.getFullYear(),
+						mm = ('0' + (d.getMonth() + 1)).slice(-2),	// Months are zero based. Add leading 0.
+						dd = ('0' + d.getDate()).slice(-2),			// Add leading 0.
+						hh = d.getHours(),
+						h = hh,
+						min = ('0' + d.getMinutes()).slice(-2),		// Add leading 0.
+						ampm = 'AM',
+						time;
+							
+					if (hh > 12) {
+						h = hh - 12;
+						ampm = 'PM';
+					} else if (hh === 12) {
+						h = 12;
+						ampm = 'PM';
+					} else if (hh == 0) {
+						h = 12;
+					}
+					
+					// ie: 2013-02-18, 8:35 AM	
+					time = dd;
+						
+					return time;
 				}
 
 			// Make the AJAX request
@@ -73,13 +100,13 @@ $(document).ready(function(){
 			$("#chanceOfRain").append("<span class='subtext'>Chance of rain</span>");
 
 			// Function to append a certain amount of hours of forecast
-			var forecaster = function(number){
+			var hourForecaster = function(number){
 				for (var i = 0; i < number; i++){
-					var time = convertTimestamp(data.hourly.data[i].time);
+					var time = UNIXtoHour(data.hourly.data[i].time);
 					var icon = data.hourly.data[i].icon;
 					var temperature = parseInt((5/9) * (data.hourly.data[i].temperature - 32));
 					var wind = (data.hourly.data[i].windSpeed * 1.61).toFixed(1);
-					$(".forecast").append(
+					$("#forecast-hour").append(
 						"<div class='col-xs-4'><p><strong>" + time + "</strong></p>" + 
 						"<p><i class='wi wi-forecast-io-" + icon + "'></i></p>" + 
 						"<p>" + temperature + "<i class='wi wi-celsius'></i>" +"</p>" + 
@@ -88,7 +115,29 @@ $(document).ready(function(){
 
 					}
 				}
-			forecaster(6);
+
+			// Function to append a certain amount of hours of forecast
+			var dayForecaster = function(number){
+				for (var i = 0; i < number; i++){
+					var day = UNIXtoDay(data.daily.data[i].time);
+					var icon = data.daily.data[i].icon;
+					var temperatureMax = parseInt((5/9) * (data.daily.data[i].temperatureMax - 32));
+					var temperatureMin = parseInt((5/9) * (data.daily.data[i].temperatureMin - 32));
+					var wind = (data.daily.data[i].windSpeed * 1.61).toFixed(1);
+					var summary = data.daily.data[i].summary;
+					$("#forecast-daily").append(
+						"<div class='col-xs-6'><p><strong>" + day + "<sup>th</sup></strong></p>" + 
+						"<p><i class='wi wi-forecast-io-" + icon + "'></i></p>" + 
+						"<p>Max: " + temperatureMax + "<i class='wi wi-celsius'></i>" +"</p>" + 
+						"<p>Min: " + temperatureMin + "<i class='wi wi-celsius'></i>" +"</p>" + 
+						"<p><i class='wi wi-strong-wind'></i> " + wind + "km/h</p>" +
+						"<p>" + summary +"</p>" + 
+						"</div>");
+
+					}
+				}
+			hourForecaster(9);
+			dayForecaster(8);
 				} // end of AJAX success function
 			}); // end of AJAX call
 		}); // end of getCurrentPosition
